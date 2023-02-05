c2a222a0fa10ed24fcbe1dd67a3b01ecf2999118
ppp <ppp.3ppj@gmail.com>
2023-02-05 18:07:44 +0700

    6 - 19 Numbers and Integer Precision

---
+++ numbers_and_integer_precision/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Numbers and Integer Precision");
+int a = 7;
+int b = 4;
+int c = 3;
+int d = (a + b) / c;
+int e = (a + b) % c;
+Console.WriteLine($"quotient: {d}");
+Console.WriteLine($"remainder: {e}");
+
+// min and max
+int max = int.MaxValue;
+int min = int.MinValue;
+Console.WriteLine($"The range of integers is {min} to {max}");
+
+// underflow and overflow
+int what = max + 3;
+Console.WriteLine($"An example of overflow: {what}");
+
+// Double Single Precision and Double Precision
+double f = 5;
+double g = 4;
+double h = 2;
+double i = (f + g) / h;
+Console.WriteLine(i);
+
+// Double min and max
+double max_double = double.MaxValue;
+double min_double = double.MinValue;
+Console.WriteLine($"The range of double is {min_double} to {max_double}");
+
+double third = 1.0 / 3.0;
+Console.WriteLine(third);
