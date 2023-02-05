2f1eb653cbd3905a1fd6054f8f14ec0a68191843
ppp <ppp.3ppj@gmail.com>
2023-02-05 22:57:12 +0700

    8 - 19 Branches (if)

---
+++ branches_if/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace ConsoleApp6 {
+  class Program {
+    static void Main(string[] args) {
+      int a = 5;
+      int b = 3;
+      int c = 4;
+      bool something = (a + b + c > 10) || (a == b);
+      if(something) {
+        Console.WriteLine("The answer is greater than 10.");
+        Console.WriteLine("Add the first number is equal to the second");
+      }
+      else {
+        Console.WriteLine("The answer is not greater then 10");
+        Console.WriteLine("The first number is not equal to the second");
+      }
+    }
+  }
+}
