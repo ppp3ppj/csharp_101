f1f36e81d737633c5b7ea459d64add6743d6c034
ppp <ppp.3ppj@gmail.com>
2023-02-05 17:53:32 +0700

    5 -19 Numbers and Integer  Math

---
+++ numbers_and_integer_math/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Numbers and Integer Match");
+int a = 18;
+int b = 6;
+int c = a + b;
+Console.WriteLine($"Result a + b is {c}");
+c = a * b;
+Console.WriteLine($"Result a * b is {c}");
+c = a / b;
+Console.WriteLine($"Result a / b is {c}");
+
+// Order of Operations
+a = 5;
+b = 4;
+c = 2;
+int d = a + b * c;
+Console.WriteLine(d);
+// or
+d = (a + b) * c;
+Console.WriteLine(d);
+
+// many diff operations
+d = (a + b) - 6 * c + (12 * 4) / 3 + 12;
+Console.WriteLine(d);
+
+// Integer Division always interger result not float or double
+a  = 7;
+b = 4;
+c = 3;
+d = (a + b) / c;
+Console.WriteLine(d);
