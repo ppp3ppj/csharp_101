d23e3377e8ba15558d939c635318b26ad637713a
ppp <ppp.3ppj@gmail.com>
2023-02-06 00:02:20 +0700

    10 - 19 What are Loops

---
+++ what_are_loops/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace ConsoleApp6 {
+  class Program {
+    static void Main(string[] args) {
+      int counter = 0;
+      int while_counter = 0;
+      while(while_counter < 10) {
+        Console.WriteLine($"The while counter is {while_counter}");
+        while_counter++;
+      }
+     for(int index = 0; index < 10; index++) {
+       Console.WriteLine($"Hello World! The index is {index}");
+     }
+
+      do {
+        Console.WriteLine($"Hello World! The counter is {counter}");
+        counter++;
+      } while(counter < 10);
+
+    }
+  }
+}
