0f7ce80b94ad5600ada7265b53f367813a569289
ppp <ppp.3ppj@gmail.com>
2023-02-05 17:42:35 +0700

    4 - 19 Searching Strings

---
+++ searching_strings/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Contains
+string songLyrics = "You say greetings, and I say Hello";
+
+var result = songLyrics.Contains("goodbye");
+Console.WriteLine(result);
+Console.WriteLine(songLyrics.Contains("greetings"));
+
+// StartsWith, EndsWith
+var resultStartsWith = songLyrics.StartsWith("You");
+var resultEndsWith = songLyrics.EndsWith("greetings");
+Console.WriteLine($"StartsWith result is {resultStartsWith}");
+Console.WriteLine($"EndsWith result is {resultEndsWith}");
