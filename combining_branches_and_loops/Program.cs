2e924f778254d7941a99936b2e12bdfd0ec93271
ppp <ppp.3ppj@gmail.com>
2023-02-06 00:34:25 +0700

    11 - 19 Combining Branches and Loops

---
+++ combining_branches_and_loops/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace ConsoleApp6 {
+  class Program {
+    static void Main(string[] args) {
+      int sum = 0;
+      for(int i = 0; i <= 20; i++) {
+        if(i % 3 == 0) {
+          sum += i;
+        }
+      }
+      Console.WriteLine($"Result is {sum}");
+    }
+  }
+}
