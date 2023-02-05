ef69c7f86527f7b4b52f3d90aac19d427eecbac5
ppp <ppp.3ppj@gmail.com>
2023-02-05 23:06:37 +0700

    9 - 19 Hello World Explained

---
+++ hello_world_explained/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace ConsoleApp6 {
+  class Program {
+    static void Main(string[] args) {
+      System.Console.WriteLine("Hello world");
+    }
+  }
+}
