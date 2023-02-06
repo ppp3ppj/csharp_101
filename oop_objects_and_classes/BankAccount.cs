6d8e8d0878891103f68e951ae27f42405c04dd36
ppp <ppp.3ppj@gmail.com>
2023-02-06 23:44:10 +0700

    17 - 19 Methods and Members

--- oop_objects_and_classes/BankAccount.cs
+++ oop_objects_and_classes/BankAccount.cs
@@ -6,11 +6,24 @@ namespace MySuperBank {
   public class BankAccount {
     public string Number { get; }
     public string Owner {get; set;}
-    public decimal Balance { get; }
+    public decimal Balance {
+      get {
+        decimal balace = 0;
+        foreach(var item in allTransactions) {
+          balace += item.Amount;
+        }
+        return balace;
+      }
+    }
+
+    private static int accountNumberSeed = 1234567890;
+
+    private List<Transaction> allTransactions = new List<Transaction>();
 
     public BankAccount(string name, decimal initialBalance) {
       this.Owner = name;
-      this.Balance = initialBalance;
+      this.Number = accountNumberSeed.ToString();
+      accountNumberSeed++;
     }
 
     public void MakeDeposit(decimal amount, DateTime date, string note) {
