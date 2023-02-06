a34c1e761f849778b9945337e88a5dbaebedaf18
ppp <ppp.3ppj@gmail.com>
2023-02-06 23:54:53 +0700

    18 - 19 OOP - Methods and Exceptions

--- oop_objects_and_classes/BankAccount.cs
+++ oop_objects_and_classes/BankAccount.cs
@@ -22,14 +22,28 @@ namespace MySuperBank {
 
     public BankAccount(string name, decimal initialBalance) {
       this.Owner = name;
+      MakeDeposit(initialBalance, DateTime.Now, "Initial Balance");
       this.Number = accountNumberSeed.ToString();
       accountNumberSeed++;
     }
 
     public void MakeDeposit(decimal amount, DateTime date, string note) {
+      if(amount <= 0) {
+        throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+      }
+      var deposit = new Transaction(amount, date, note);
+      allTransactions.Add(deposit);
 
     }
     public void MakeWithdrawal(decimal amount, DateTime date, string note) {
+      if(amount <= 0) {
+        throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive");
+      }
+      if(Balance - amount < 0) {
+        throw new InvalidOperationException("Not sufficient funds for this withdrawal");
+      }
+      var withdrawal = new Transaction(-amount, date, note);
+      allTransactions.Add(withdrawal);
 
     }
 
--- oop_objects_and_classes/Program.cs
+++ oop_objects_and_classes/Program.cs
@@ -3,8 +3,10 @@ using System;
 namespace MySuperBank {
   class Program {
     static void Main(string[] args) {
-      var account = new BankAccount("ppp", 1000);
+      var account = new BankAccount("ppp", 10000);
       Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance}");
+      account.MakeWithdrawal(120, DateTime.Now, "Hammock");
+      Console.WriteLine(account.Balance);
     }
   }
 }
