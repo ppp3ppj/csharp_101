7fd7da6be46cc493e334c01b082c4066521b9bdf
ppp <ppp.3ppj@gmail.com>
2023-02-06 21:46:26 +0700

    14 - 19 Lists of Other Types

---
+++ lists_of_other_types/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+
+namespace list_tutorial {
+  class Program {
+    static void Main(string[] args) {
+     var fibonacciNumbers = new List<int> { 1, 1};
+
+     while(fibonacciNumbers.Count < 20) {
+      var previous = fibonacciNumbers[fibonacciNumbers.Count -1];
+      var previous2 = fibonacciNumbers[fibonacciNumbers.Count -2];
+
+      fibonacciNumbers.Add(previous + previous2);
+     }
+     foreach(var item in fibonacciNumbers) {
+       Console.WriteLine(item);
+     }
+    }
+  }
+}
