431dccfe9f4a3cd921be34c429cbf996818b2c96
ppp <ppp.3ppj@gmail.com>
2023-02-06 23:31:29 +0700

    16 - 19 OOP - Objects and Classes

---
+++ oop_objects_and_classes/BankAccount.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MySuperBank {
+  public class BankAccount {
+    public string Number { get; }
+    public string Owner {get; set;}
+    public decimal Balance { get; }
+
+    public BankAccount(string name, decimal initialBalance) {
+      this.Owner = name;
+      this.Balance = initialBalance;
+    }
+
+    public void MakeDeposit(decimal amount, DateTime date, string note) {
+
+    }
+    public void MakeWithdrawal(decimal amount, DateTime date, string note) {
+
+    }
+
+  }
+}
---
+++ oop_objects_and_classes/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace MySuperBank {
+  class Program {
+    static void Main(string[] args) {
+      var account = new BankAccount("ppp", 1000);
+      Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance}");
+    }
+  }
+}
